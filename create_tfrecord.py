055d2671f788795c362f526ca51f24d4cb8c37a9
zhuzhu <zhuzhu18@aliyun.com>
2019-11-23 20:29:23 +0800

    创建tfrecords数据格式

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -1,32 +1,22 @@
 import tensorflow as tf
-import os
-import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-def _int64_feature(value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-def _bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-mnist = input_data.read_data_sets('/media/tl/213/data/mnist', dtype=tf.uint8, one_hot=True)
-
-images = mnist.train.images
-labels = mnist.train.labels
-
+mnist = input_data.read_data_sets('/media/zhuzhu/ec114170-f406-444f-bee7-a3dc0a86cfa2/dataset/mnist')
 num_examples = mnist.train.num_examples
-img_size = images.shape[1]
-filename = '/media/tl/213/data/mnist/mnist_records.tfrecords'
-writer = tf.io.TFRecordWriter(filename)
-for index in range(num_examples):
-    image_raw = images[index].tostring()
-    example = tf.train.Example(features=tf.train.Features(
-        feature={
-            'img_size': _int64_feature(img_size),
-            'label': _int64_feature(np.argmax(labels[index])),
-            'image_raw': _bytes_feature(image_raw)
-        }
-    ))
+images = mnist.train.images    # numpy float32类型, 50000 * 784
+labels = mnist.train.labels    # numpy uint8类型, 50000
+
+file_path = './a/mnist_train.tfrecords'
+writer = tf.io.TFRecordWriter(path=file_path)
+
+for i in range(num_examples):
+    images_feature = tf.train.Feature(float_list=tf.train.FloatList(value=images[i].tolist()))
+    labels_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[labels[i].tolist()]))
+
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'images': images_feature,
+        'labels': labels_feature
+    }))
     writer.write(example.SerializeToString())
+
 writer.close()
